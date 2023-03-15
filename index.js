90f85d7afce4d3087e319ada564d24f062cef98a
Eliasvintra <rbnvintra.ke@gmail.com>
2023-03-15 18:57:52 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+//solution here
+function distanceFromHqInBlocks(pick_up_location){
+    const hq_location = 42;
+    const distance_in_block = Math.abs(pick_up_location - hq_location);
+    return distance_in_block;
+}
+
+function distanceFromHqInFeet(pick_up_location) {
+    const feet_per_block =264;
+    const distance_in_block = distanceFromHqInBlocks(pick_up_location);
+    const distance_in_feet = distance_in_block * feet_per_block;
+    return distance_in_feet;
+}
+
+function distanceTravelledInFeet(start, end) {
+    const feet_per_block =264;
+    const distance_in_block = Math.abs(start - end);
+    const distance_in_feet = distance_in_block * feet_per_block;
+    return distance_in_feet;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    let fare = 0;
+
+if (distance <= 400) {
+    fare = 0; 
+} else if (distance > 400 && distance <= 2000) {
+    fare = (distance - 400) * 0.02;
+} else if (distance > 2000 && distance <= 2500) {
+    fare = 25; 
+} else {
+    fare = 'cannot travel that far';
+}
+return fare;
+}
+
--- test/indexTest.js
+++ test/indexTest.js
@@ -60,3 +60,39 @@ describe('index.js', function() {
     });
   });
 });
+//////////////////////////////////////
+
+/*function distanceFromHqInBlocks(pickupLocation) {
+  const hqLocation = 42;
+  const distanceInBlocks = Math.abs(pickupLocation - hqLocation);
+  return distanceInBlocks;
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+  const feetPerBlock = 264;
+  const distanceInBlocks = distanceFromHqInBlocks(pickupLocation);
+  const distanceInFeet = distanceInBlocks * feetPerBlock;
+  return distanceInFeet;
+}
+function distanceTravelledInFeet(startBlock, endBlock) {
+  const feetPerBlock = 264;
+  const distanceInBlocks = Math.abs(startBlock - endBlock);
+  const distanceInFeet = distanceInBlocks * feetPerBlock;
+  return distanceInFeet;
+}
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  let fare = 0;
+  
+  if (distance <= 400) {
+    fare = 0;
+  } else if (distance > 400 && distance <= 2000) {
+    fare = (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    fare = 25;
+  } else {
+    fare = 'cannot travel that far';
+  }
+  
+  return fare;
+}*/
